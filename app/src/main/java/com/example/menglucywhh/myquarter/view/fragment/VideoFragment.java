282c44b8b2a17b64219837b2742f054ed8cf7c81
2679923982 <2679923982@qq.com>
2018-01-27 11:14:12 +0800

    hong

--- app/src/main/java/com/example/menglucywhh/myquarter/view/fragment/VideoFragment.java
+++ app/src/main/java/com/example/menglucywhh/myquarter/view/fragment/VideoFragment.java
@@ -4,14 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.androidkun.xtablayout.XTabLayout;
+import com.example.menglucywhh.myquarter.MainActivity;
 import com.example.menglucywhh.myquarter.R;
+import com.example.menglucywhh.myquarter.utils.CustomViewPager;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,79 +30,72 @@ import butterknife.Unbinder;
 
 public class VideoFragment extends Fragment {
 
-    @BindView(R.id.mine)
-    RadioButton mine;
-    @BindView(R.id.news)
-    RadioButton news;
-    @BindView(R.id.group)
-    RadioGroup group;
+
+    @BindView(R.id.touxiang)
+    SimpleDraweeView touxiang;
+    @BindView(R.id.top_text)
+    TextView topText;
+    @BindView(R.id.edit)
+    ImageView edit;
+    @BindView(R.id.aaa)
+    RelativeLayout aaa;
+    @BindView(R.id.xTablayout)
+    XTabLayout xTablayout;
     @BindView(R.id.pager)
-    ViewPager pager;
+    CustomViewPager pager;
     Unbinder unbinder;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_video, container, false);
+
+
         unbinder = ButterKnife.bind(this, view);
-
-        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {//fragment滑动
-                switch (i){
-                    case R.id.mine:
-                        pager.setCurrentItem(0,false);
-                        break;
-                    case R.id.news:
-                        pager.setCurrentItem(1,false);
-                        break;
-
-                }
-            }
-        });
-//        tablayout.setupWithViewPager(viewPager);
-        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {//viewpager点击
-
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-
-            public void onPageSelected(int position) {
-                group.check(group.getChildAt(position).getId());
-            }
-
-
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-        pager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {
-                Fragment fragment=null;
-                switch (position){
-                    case 0:
-                        fragment=new Video_remenFragment();
-                        break;
-                    case 1:fragment=new Video_fujinFragment();
-                        break;
-
-                }
-                return fragment;
-            }
-
-
-            public int getCount() {
-                return 2;
-            }
-        });
         return view;
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        //第一个条目的点击事件
+        touxiang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MainActivity.touClick();
+            }
+        });
+        final List<String> list = new ArrayList<>();
+        list.add("热门");
+        list.add("附近");
+
+        pager.setOffscreenPageLimit(list.size());//预加载
+        pager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return list.get(position);//和tablayout结合使用
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                Fragment fragment = null;
+                if (position == 0) {
+                    fragment = new Video_remenFragment();//热门的fragment
+                } else {
+                    fragment = new Video_remenFragment();//关注的fragment
+                }
+                return fragment;
+            }
+
+            @Override
+            public int getCount() {
+                return list.size();
+            }
+        });
+        //设置viewpager和Xtablayout联动
+        xTablayout.setupWithViewPager(pager);
 
     }
 
